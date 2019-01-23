c3925d4065c6292adf1bfb620fccbdfb9a25b496
Jessica Burnett <TrashBirdEcology@users.noreply.github.com>
2019-01-23 13:05:56 -0600

    Update calculateMetrics.R

--- R/calculateMetrics.R
+++ R/calculateMetrics.R
@@ -5,72 +5,85 @@
 #' @param metrics.to.calc One or more of c("distances", "ews")
 #' @export calculateMetrics
 
-calculateMetrics <- function(dataIn = dataIn, metrics.to.calc = c("distances", "ews", min.samp.sites = 8)
-){
-
-     # keep only the relevant columns
-     dataIn  <- dataIn %>%
-     dplyr::select(
-         time, variable, value) %>%
-     # add up any species having multiple observations -- this happens esp. for hybrids! is common.
-     group_by(variable, time) %>%
-     summarise(value = sum(value)) %>%
-     ungroup()
-
-
-flag <- ifelse(length(unique(dataIn$time)) < min.samp.sites, 'stop', 'go')
+calculateMetrics <-
+    function(dataIn = dataIn,
+             metrics.to.calc = c("distances", "ews"),
+             min.samp.sites = 8) {
+        ## Munge the input data a little --this is a failsafe!
+        # keep only the relevant columns
+        dataIn  <- dataIn %>%
+            dplyr::select(time, variable, value) %>%
+            # add up any species having multiple observations -- this happens esp. for hybrids! is common.
+            group_by(variable, time) %>%
+            summarise(value = sum(value)) %>%
+            ungroup()
 
 
-if("distances" %in% metrics.to.calc & flag == "go") {
-    metricInd = "distances"
-    # keep only the relevant columns
-    dataIn  <- dataIn %>%
-        dplyr::select(
-            time, variable, value)
-    if(!length(unique(dataIn$time)) < min.samp.sites){
-
-    # Calc distances
-    results <- NULL
-    results <- calculate_distanceTravelled(dataIn, derivs = T) %>%
-        gather(key = 'metricType', value ='metricValue', -time)
-
-
-    saveMyResults(results ,resultsDir =resultsDir, analySpatTemp =analySpatTemp, metricInd = metricInd)
-
-    }
+        if(length(unique(dataIn$time)) < min.samp.sites){
+             flag = "# dataIn$time points < min.samp.sites. Not calculating metrics. "
+            return(flag)
 }
 
-# f.ii.a Calculate the EWSs
-if ("ews" %in% metrics.to.calc & flag == "go") {
-    metricInd = "ews"
+        ## Calculate distance traveled
+        if ("distances" %in% metrics.to.calc) {
+            metricInd = "distances"
+            # keep only the relevant columns
+            dataIn  <- dataIn %>%
+                dplyr::select(time, variable, value)
+            if (!length(unique(dataIn$time)) < min.samp.sites) {
+                # Calc distances
+                results <- NULL
+                results <- calculate_distanceTravelled(dataIn, derivs = T) %>%
+                    gather(key = 'metricType', value = 'metricValue',-time)
 
-    # keep only the relevant columns
-    dataIn  <- dataIn %>%
-        dplyr::select(
-            time, variable, value) %>%
-        # add up any species having multiple observations -- this happens esp. for hybrids! is common.
-        group_by(variable, time) %>%
-        summarise(value = sum(value)) %>%
-        ungroup()
 
-    results <- NULL
-    results <-
-        rdm_window_analysis(
-            dataInRDM = dataIn %>%
-                # arrange the data in temporal (spatial) order
-                dplyr::group_by(variable) %>%
-                arrange(variable, time) %>%
-                ungroup(),
-            winMove = winMove,
-            overrideSiteErr = F,
-            fi.equation = fi.equation,
-            min.window.dat = min.window.dat,
-            fill = fill,
-            to.calc = to.calc)
+                saveMyResults(
+                    results ,
+                    resultsDir = resultsDir,
+                    analySpatTemp = analySpatTemp,
+                    metricInd = metricInd
+                )
 
-    saveMyResults(results , resultsDir =resultsDir, analySpatTemp =analySpatTemp, metricInd = metricInd)
+            }
+        }
 
-    } # leave EWS calculations
 
-} # leave function
+        ## Calculate early-warning signals
+        if ("ews" %in% metrics.to.calc) {
+            metricInd = "ews"
 
+            # keep only the relevant columns
+            dataIn  <- dataIn %>%
+                dplyr::select(time, variable, value) %>%
+                # add up any species having multiple observations -- this happens esp. for hybrids! is common.
+                group_by(variable, time) %>%
+                summarise(value = sum(value)) %>%
+                ungroup()
+
+            results <- NULL
+            results <-
+                rdm_window_analysis(
+                    dataInRDM = dataIn %>%
+                        # arrange the data in temporal (spatial) order
+                        dplyr::group_by(variable) %>%
+                        arrange(variable, time) %>%
+                        ungroup(),
+                    winMove = winMove,
+                    overrideSiteErr = F,
+                    fi.equation = fi.equation,
+                    min.window.dat = min.window.dat,
+                    fill = fill,
+                    to.calc = to.calc
+                )
+
+            saveMyResults(
+                results ,
+                resultsDir = resultsDir,
+                analySpatTemp = analySpatTemp,
+                metricInd = metricInd
+            )
+
+        } # leave EWS calculations
+
+
+    } # leave function
