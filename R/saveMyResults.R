17ccaa96fef3243f181297b4cdd31b79bc8365f7
Jessica Burnett <jessicaleighburnett@gmail.com>
2019-01-25 18:47:51 -0600

    Update saveMyResults.R

--- R/saveMyResults.R
+++ R/saveMyResults.R
@@ -2,19 +2,16 @@
 #' @description Writes the results of distance travelled to file as .feather.
 #' @param results A data frame or list element with columns 'time', 'metricType', and 'metricValue' for either the EWS or Distance results.
 #' @param resultsDir Where to save the feather.
-#' @param yearInd Year of analysis.
 #' @param metricInd One of 'distances' or 'ews'. Used in outfile name.
 
 
 saveMyResults <- function(results,
                           resultsDir,
-                          metricInd,
-                          yearInd
+                          metricInd
                           ) {
 
 
-# browser()
-    fileNameInd = paste0(Sys.Date(),"_year", yearInd,  "_", direction,  "_transect", unique(results$dirID))
+    fileNameInd = paste0(Sys.Date(),"_year", unique(results$year),  "_", direction,  "_transect", unique(results$dirID))
 
     ## WRITE TO FILE ##
     # Save results as .feather
