ee9dd1b603dd76420bb634f9eba84a741ce10453
Jessica Burnett <jessicaleighburnett@gmail.com>
2019-05-19 19:44:47 -0500

    add package documentation

---
+++ R/bbsRDM.r
@@ -0,0 +1,11 @@
+#' bbsRDM: A package for calculating various 'regime detection' measures.
+#'
+#' The bbsRDM package provides source code for calculating multiple methods that are used in the ecological regime shift literatures.
+#' foo, bar and baz.
+#'
+#' @section bbsRDM functions:
+#'
+#'
+#' @docType package
+#' @name bbsRDM
+NULL
