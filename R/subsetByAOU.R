bbadeb7efcaa29d4b4cefba17819a9e22efc3c9b
Jessica Burnett <TrashBirdEcology@users.noreply.github.com>
2019-05-22 10:07:00 -0500

    Add option to remove by a vector of AOU codes (numeric)

--- R/subsetByAOU.R
+++ R/subsetByAOU.R
@@ -2,10 +2,12 @@
 #' @title Subset the BBS data by species, functional traits, and/or body mass.
 #' @param subset.by One or more of 'remove.fowl' (removes waterfowl), "remove.shorebirds" (removes shorebirds and waders),  'remove.shoreWaderFowl' (removes shorebirds, waders, and fowl).
 #' @param mass Logical. Retrieves body mass information (Dunning reference).
+#' @param aou.codes Numeric or vector of numeric values of the AOU codes. These are the species you want to REMOVE from analysis.
 #' @export subsetByAOU
 
-
-subsetByAOU <- function(myData, subset.by = c( 'remove.fowl', "remove.shorebirds",  'remove.shoreWaderFowl')){
+subsetByAOU <- function(myData, subset.by = c( 'remove.fowl', "remove.shorebirds",  'remove.shoreWaderFowl'), 
+                       aou = NULL
+                       ){
     # subset.by =c("keep.diurnal", 'remove.fowl', "remove.shorebirds", "  'remove.shoreWaderFowl')
 
 ## The AOU species codes can be found at [the BBS FTP site]("ftp://ftpext.usgs.gov/pub/er/md/laurel/BBS/DataFiles/SpeciesList.txt").
@@ -30,6 +32,12 @@ subsetByAOU <- function(myData, subset.by = c( 'remove.fowl', "remove.shorebirds
             filter(!aou %in% c(0000:2880))
     }
 
+    
+    ## Subset by AOU specific numbers. 
+     if(!is.null(aou.codes)){
+        myData <- myData %>%
+            filter(!aou %in% aou.codes)
+    }
 
     return(myData)
 
